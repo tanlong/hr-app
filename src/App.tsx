07844a5513b20923e2e59be759b7950614b8c9de
longdt <duongtanlong@vietbank.com.vn>
2023-03-23 16:10:42 +0700

    unit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import { PrivateRoute } from "./components/router";
+import { Login } from "./pages/Account";
+import { Admin } from "./pages/Admin";
+import "./styles/sb-admin-2.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isLogin = false
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <PrivateRoute auth={isLogin} />,
+            children: [
+                {
+                    index: true,
+                    element: <Admin />,
+                },
+            ],
+        },
+        {
+            path: "/login",
+            element: <Login />,
+        },
+    ]);
+    return (
+        <div className="App" id="wrapper">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/router/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet, RouteProps } from 'react-router-dom';
+
+export const PrivateRoute = ({ auth, ...rest }: {auth:boolean}& RouteProps): JSX.Element => {
+    if (auth) {
+        return <Outlet />;
+    }
+
+    return <Navigate to='/login' replace />
+};
